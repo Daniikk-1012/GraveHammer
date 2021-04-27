0f48a00ffc69e867b3584d06f0018f9bd243d1c9
Daniikk-1012 <49123959+Daniikk-1012@users.noreply.github.com>
2021-04-27 08:49:47 +0600

    Downgrade lwjgl (Fix jar)
    Jar did not run when using lwjgl3 instead of the default

--- desktop/src/com/wgsoft/game/gravehammer/desktop/DesktopLauncher.java
+++ desktop/src/com/wgsoft/game/gravehammer/desktop/DesktopLauncher.java
@@ -1,14 +1,15 @@
 package com.wgsoft.game.gravehammer.desktop;
 
-import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
-import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.wgsoft.game.gravehammer.MyGdxGame;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
-		Lwjgl3ApplicationConfiguration config
-            = new Lwjgl3ApplicationConfiguration();
-        config.setTitle("Grave Hammer");
-		new Lwjgl3Application(MyGdxGame.getInstance(), config);
+		final LwjglApplicationConfiguration config
+            = new LwjglApplicationConfiguration();
+        config.title = "Grave Hammer";
+        config.forceExit = false;
+		new LwjglApplication(MyGdxGame.getInstance(), config);
 	}
 }
